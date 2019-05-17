7a6d56d67d0c6dc0d562ba02ee16a91a53abc1d0
Muhammad Naufal <mnaufal75@gmail.com>
2019-05-17 07:46:33 +0700

    Make site responsive

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-$("#fetchData").click(function() {
+$("#requestButton").click(function() {
   $.ajax({
     url: "http://www.mocky.io/v2/5c7cf10c1000002c15760d04",
     type: "GET",
   })
     .done(function (data) {
-      $("#data").text(data.content);
+      $("#responseText").text(data.content);
     })
 });
