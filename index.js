15fcd6e8a6417e8fc74aafde7b49403b2292c47a
Muhammad Naufal <mnaufal75@gmail.com>
2019-05-17 07:56:18 +0700

    Add text while get request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $("#requestButton").click(function() {
+  $("#responseText").text("Sedang mengambil data");
   $.ajax({
     url: "http://www.mocky.io/v2/5c7cf10c1000002c15760d04",
     type: "GET",
