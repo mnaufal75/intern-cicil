ccb16c1ad037694aa06e6be45cce5ccd81deb1db
Muhammad Naufal <mnaufal75@gmail.com>
2019-05-17 08:00:42 +0700

    Change endpoint address to https

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $("#requestButton").click(function() {
   $("#responseText").text("Sedang mengambil data");
   $.ajax({
-    url: "http://www.mocky.io/v2/5c7cf10c1000002c15760d04",
+    url: "https://www.mocky.io/v2/5c7cf10c1000002c15760d04",
     type: "GET",
   })
     .done(function (data) {
