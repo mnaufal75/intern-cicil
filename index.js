b077cd853c6b48ec59496b7bd02289a011358fc4
Muhammad Naufal <mnaufal75@gmail.com>
2019-05-17 19:29:17 +0700

    Disable button after request

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-$("#requestButton").click(function() {
-  $("#responseText").text("Sedang mengambil data");
-  $.ajax({
-    url: "https://www.mocky.io/v2/5c7cf10c1000002c15760d04",
-    type: "GET",
-  })
+$(document).ready(function () {
+  $("#requestButton").click(function() {
+    $("#responseText").text("Sedang mengambil data");
+    $.ajax({
+      url: "https://www.mocky.io/v2/5c7cf10c1000002c15760d04",
+      type: "GET",
+    })
     .done(function (data) {
       $("#responseText").text(data.content);
+      $("#requestButton").prop('disabled', true);
     })
+  });
 });
